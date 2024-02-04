3e56c3b5b4da380e14a2f4b17b7f58ddef7eca69
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2024-02-04 15:09:44 -0700

    ✨ Use `SMAppServce`; minimum MacOS is now Ventura

--- Commander/Common/Extension/NotificationName+Extension.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-extension Notification.Name {
-    static let killLauncher = Notification.Name("killLauncher")
-}
--- Commander/Manager/AutoLaunchManager.swift
+++ Commander/Manager/AutoLaunchManager.swift
@@ -3,17 +3,10 @@ import ServiceManagement
 
 final class AutoLaunchManager {
     func configureAutoLaunch(enabled: Bool) {
-        let launcherAppID = "com.va.commander.launcher"
-        let runningApps = NSWorkspace.shared.runningApplications
-        let isRunning = runningApps.contains {
-            $0.bundleIdentifier == launcherAppID
-        }
-
-        SMLoginItemSetEnabled(launcherAppID as CFString, enabled)
-
-        if isRunning {
-            DistributedNotificationCenter.default()
-                .post(name: .killLauncher, object: Bundle.main.bundleIdentifier!)
+        if enabled {
+            try? SMAppService().register()
+        } else {
+            try? SMAppService().unregister()
         }
     }
 }
